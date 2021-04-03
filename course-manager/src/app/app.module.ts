595d3425d2c382ae1cfcb6919612891e42511874
Alan Brito <alanbritodossantos2015@gmail.com>
2021-04-03 15:26:49 -0300

    Segregando a aplicação em módulos

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,45 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router'; //módulo de rotas do angular
 
 import { HttpClientModule} from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent, // referente a tag <app-nav-bar> menu
-    Error404Component,
-    CourseInfoComponent
+    Error404Component
+    // CourseInfoComponent
+
   ],
   imports: [ //tudo que é módulo é colocado aqui
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
 
     // essa array espera um objeto de rota
     RouterModule.forRoot([  // assim que inicializar já vai carragar nossas rotas
-      //array com uma serie de objetos que vai ter na aplicação
-        {//objeto de rota
-          //url da rota, de acordo com o que for digitado no browser
-          path: ''/*o path vazio, diz para o angula quando estiver na porta 4200 raiz*/, redirectTo:'courses' /*vai redirecionar para course*/, pathMatch:'full'
-        },
-        {
-          path:'courses/info/:id', component: CourseInfoComponent
-        },
         {//listar
-          path:'courses', component:CourseListComponent
+          path:'', redirectTo:'courses', pathMatch:'full'
         },
         {
           path:'**', component: Error404Component /*o '**' signigica quando não encontrar uma rota(url)*/
