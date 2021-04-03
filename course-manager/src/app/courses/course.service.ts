3005f4d79f999a0df7222cdd76a959c9468825f9
Alan Brito <alanbritodossantos2015@gmail.com>
2021-04-03 01:14:55 -0300

    fazendo requisição em HTTP

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -8,23 +8,26 @@ import { Observable } from 'rxjs';
 })
 export class CourseService {
 
-  private courseUrl: string = 'http://localhost:3100/api/courses';
+  private coursesUrl: string = 'http://localhost:3100/api/courses';
 
   constructor(private httpClient: HttpClient){}
 
     retrieveAll(): Observable<Course[]> {
-      return this.httpClient.get<Course[]>(this.courseUrl);
+      return this.httpClient.get<Course[]>(this.coursesUrl);
     }
 
-    retrieveById(id: number): Course{
+    retrieveById(id: number): Observable<Course>{
       //filtrando array-- o find vai analisar o array e buscar a condição que é solicitado comparando 'ID'
-        return COURSES.find((courseIterator:Course) => courseIterator.id === id);
+        return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
     }
 
-    save(course: Course): void{
+    save(course: Course): Observable<Course>{
         if(course.id){
-          const index = COURSES.findIndex((courseIterator:Course)=> courseIterator.id===course.id)
-          COURSES[index] = course;
+          // const index = COURSES.findIndex((courseIterator:Course)=> courseIterator.id===course.id)
+          // COURSES[index] = course;
+          return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`,course);
+        } else{
+          return this.httpClient.post<Course>(`${this.coursesUrl}`,course);
         }
     }
 }
