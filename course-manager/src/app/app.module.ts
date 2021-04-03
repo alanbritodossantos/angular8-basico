504c1e76cc961668710d19482ad62dc33d12f6b2
Alan Brito <alanbritodossantos2015@gmail.com>
2021-04-03 20:28:25 -0300

    adicionando pasta core

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,24 +4,21 @@ import { RouterModule } from '@angular/router'; //módulo de rotas do angular
 
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent, // referente a tag <app-nav-bar> menu
     Error404Component
-    // CourseInfoComponent
-
   ],
   imports: [ //tudo que é módulo é colocado aqui
     BrowserModule,
     HttpClientModule,
     CourseModule,
-
+    CoreModule,
     // essa array espera um objeto de rota
     RouterModule.forRoot([  // assim que inicializar já vai carragar nossas rotas
         {//listar
